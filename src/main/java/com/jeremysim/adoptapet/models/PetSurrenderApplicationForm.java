bdd6c420284ad013640dfd58bfd0baec18a65e0e
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-10-06 13:04:38 -0700

    Implement validation

--- src/main/java/com/jeremysim/adoptapet/models/PetSurrenderApplicationForm.java
+++ src/main/java/com/jeremysim/adoptapet/models/PetSurrenderApplicationForm.java
@@ -1,5 +1,7 @@
 package com.jeremysim.adoptapet.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
@@ -8,13 +10,22 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class PetSurrenderApplicationForm {
+  @NotBlank
   private String name;
+  @NotBlank
   private String phoneNumber;
+  @NotBlank
   private String email;
+  @NotBlank
   private String petName;
+
   private Integer petAge;
+  @NotNull
   private Integer surrenderedPetType;
+  @NotBlank
   private String petImageUrl;
+
   private Boolean vaccinationStatus;
+  @NotBlank
   private String applicationStatus;
 }
